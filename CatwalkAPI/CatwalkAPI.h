e7d7e809087b1287f67a992d453a966d7fedbf81
Fernnando <frontend@mycatwalk.com>
2021-08-12 17:12:05 -0300

    Initial Commit

---
+++ CatwalkAPI/CatwalkAPI.h
@@ -0,0 +1,19 @@
+//
+//  CatwalkAPI.h
+//  CatwalkAPI
+//
+//  Created by CTWLK on 8/12/21.
+//  Copyright © 2021 CATWALK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatwalkAPI.
+FOUNDATION_EXPORT double CatwalkAPIVersionNumber;
+
+//! Project version string for CatwalkAPI.
+FOUNDATION_EXPORT const unsigned char CatwalkAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatwalkAPI/PublicHeader.h>
+
+
